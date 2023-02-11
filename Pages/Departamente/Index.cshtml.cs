6c4519814cc63c056b4c41e8f3e3e560db9816cc
andreimoisin <98484063+andreimoisin@users.noreply.github.com>
2023-02-11 20:16:26 +0200

    Updated Departamente page to display the activities of a departament on a selected departament.

--- Pages/Departamente/Index.cshtml.cs
+++ Pages/Departamente/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ActivitatiVoluntariatWEB.Data;
 using ActivitatiVoluntariatWEB.Models;
+using ActivitatiVoluntariatWEB.Models.ViewModels;
 
 namespace ActivitatiVoluntariatWEB.Pages.Departamente
 {
@@ -21,8 +22,26 @@ namespace ActivitatiVoluntariatWEB.Pages.Departamente
 
         public IList<Departament> Departament { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public DepartamentIndexData DepartamentData { get; set; }
+        public int DepartamentID { get; set; }
+        public int ActivitateID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? activitateID)
         {
+            DepartamentData = new DepartamentIndexData();
+            DepartamentData.Departamente = await _context.Departament
+                .Include(i => i.Activitati)
+                .ToListAsync();
+
+            if(id != null)
+            {
+                DepartamentID = id.Value;
+                Departament dep = DepartamentData.Departamente
+                    .Where(i => i.ID == id.Value).Single();
+                DepartamentData.Activitati = dep.Activitati;
+            }
+
+
             if (_context.Departament != null)
             {
                 Departament = await _context.Departament.ToListAsync();
