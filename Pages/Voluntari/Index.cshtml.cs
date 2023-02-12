5f7a7acdba28543f7516c5909e0846c87756e171
andreimoisin <98484063+andreimoisin@users.noreply.github.com>
2023-02-12 18:18:02 +0200

    Added sort and search options on volunteers page.

--- Pages/Voluntari/Index.cshtml.cs
+++ Pages/Voluntari/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ActivitatiVoluntariatWEB.Data;
 using ActivitatiVoluntariatWEB.Models;
+using Microsoft.Data.SqlClient;
 
 namespace ActivitatiVoluntariatWEB.Pages.Voluntari
 {
@@ -19,15 +20,59 @@ namespace ActivitatiVoluntariatWEB.Pages.Voluntari
             _context = context;
         }
 
+        public string DepartamentSortOrder { get; set; }
+
+        public string NumeSortOrder { get; set; }
+
         public IList<Voluntar> Voluntar { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetSortByNameAsync()
         {
-            if (_context.Voluntar != null)
+            var voluntari = _context.Voluntar.Include(a => a.Departament).AsQueryable();
+
+            voluntari = voluntari.OrderBy(a => a.Nume);
+            return Page();
+        }
+
+        public async Task OnGetAsync(string sortOrder, string searchString)
+        {
+            DepartamentSortOrder = sortOrder == "dep_asc" ? "dep_desc" : "dep_asc";
+
+            NumeSortOrder = sortOrder == "nume_asc" ? "nume_desc" : "nume_asc";
+
+            var voluntari = _context.Voluntar
+                .Include(a => a.Departament)
+                .AsQueryable();
+
+            switch (sortOrder)
             {
-                Voluntar = await _context.Voluntar
-                .Include(v => v.Departament).ToListAsync();
+                case "nume_desc":
+                    voluntari = voluntari.OrderByDescending(a => a.Nume);
+                    break;
+                case "nume_asc":
+                    voluntari = voluntari.OrderBy(a => a.Nume);
+                    break;
+                case "dep_desc":
+                    voluntari = voluntari.OrderByDescending(a => a.Departament.NumeDepartament);
+                    break;
+                case "dep_asc":
+                    voluntari = voluntari.OrderBy(a => a.Departament.NumeDepartament);
+                    break;
+                default:
+                    voluntari = voluntari.OrderBy(a => a.Nume);
+                    break;
             }
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                voluntari = voluntari.Where(a => a.Nume.Contains(searchString) ||
+                                                   a.Prenume.Contains(searchString) ||
+                                                   a.Departament.NumeDepartament.Contains(searchString));
+            }
+
+            Voluntar = await voluntari
+            .Include(a => a.Departament)
+            .ToListAsync();
         }
     }
 }
